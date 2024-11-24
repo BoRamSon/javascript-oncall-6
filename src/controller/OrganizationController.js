361ea3ecbc6a3e04d128fa0569104c676dadfb9d
BoRamSon <onbrson1@gmail.com>
2024-11-24 17:26:58 +0900

    feat(controller): 평일 비상 근무 순서를 입력받는다.

--- src/controller/OrganizationController.js
+++ src/controller/OrganizationController.js
@@ -8,6 +8,23 @@ class OrganizationController {
   async organizeScheduling() {
     await this.inputMonthAndDay.changeArray();
   }
+
+  // 평일 비상 근무 순서를 입력받는다.
+  async inputWeekdayWorker() {
+    const weekdayWorker = await MissionUtils.Console.readLineAsync(
+      "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> "
+    );
+  }
+
+  validationWeekday() {
+    
+  }
+
+  // 휴일(토요일, 일요일, 법정공휴일) 비상 근무 순서를 입력받는다.
+  async inputWeekendWorker() {
+
+  }
+
 }
 
 export default OrganizationController;
