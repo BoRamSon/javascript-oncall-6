413a0be484dca546fe459ee834a7f4d60711c9ee
BoRamSon <onbrson1@gmail.com>
2024-11-24 16:47:15 +0900

    feat(controller): 올바르지 않은 입력을 할 경우 [ERROR]로 시작하는 에러 메시지를 출력 후 다시 입력받는다.

--- src/controller/OrganizationController.js
+++ src/controller/OrganizationController.js
@@ -1,17 +1,20 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import validateMonthAndDay from "../validation/validateMonthAndDay.js";
 
 class OrganizationController {
   constructor() {}
 
-  organizeScheduling() {
-    this.getMonthAndDay();
+  async organizeScheduling() {
+    await this.getMonthAndDay();
   }
 
-  getMonthAndDay() {
-    const inputMonthAndDay = MissionUtils.Console.readLineAsync(
+  async getMonthAndDay() {
+    const inputMonthAndDay = await MissionUtils.Console.readLineAsync(
       "비상 근무를 배정할 월과 시작 요일을 입력하세요> "
     );
-    
+    const stringType = String(inputMonthAndDay);
+    const noSpace = stringType.replace(/ /g, "");
+    return validateMonthAndDay(noSpace);
   }
 
 
