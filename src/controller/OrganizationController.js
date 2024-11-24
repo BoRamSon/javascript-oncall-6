1dd08d82bc71ab38681178077efd98dfbf6eaa5c
BoRamSon <onbrson1@gmail.com>
2024-11-24 17:08:52 +0900

    feat(controller): 비상 근무를 배정할 월과 시작 요일의 입력 값이 올바르지 않은 경우, '비상 근무를 배정할 월과 시작 요일'부터 다시 입력받는다.

--- src/controller/OrganizationController.js
+++ src/controller/OrganizationController.js
@@ -5,16 +5,38 @@ class OrganizationController {
   constructor() {}
 
   async organizeScheduling() {
-    await this.getMonthAndDay();
+    await this.inputAndValidation();
   }
 
-  async getMonthAndDay() {
-    const inputMonthAndDay = await MissionUtils.Console.readLineAsync(
+  async inputMonthAndDay() {
+    const monthAndDay = await MissionUtils.Console.readLineAsync(
       "비상 근무를 배정할 월과 시작 요일을 입력하세요> "
     );
-    const stringType = String(inputMonthAndDay);
-    const noSpace = stringType.replace(/ /g, "");
-    return validateMonthAndDay(noSpace);
+    const stringType = String(monthAndDay);
+    const noSpaceValue = stringType.replace(/ /g, "");
+    validateMonthAndDay(noSpaceValue);
+    return noSpaceValue;
+  }
+
+  
+  async inputAndValidation() {
+    try {
+      const validatedInput = await this.inputMonthAndDay();
+      return validatedInput;
+    } catch (error) {
+      MissionUtils.Console.print(error);
+      return false;
+    }
+  }
+
+  async repeatUntilCorrect() {
+    for(let i = 0; i < 10; i++){
+      const validatedInput = this.inputAndValidation();
+      if(validatedInput) return validatedInput;
+      
+      if(i === 5) 
+        throw new Error("5번 이상 잘 못 입력하여 종료되었습니다. 처음부터 다시 시작해 주세요.")
+    }
   }
 
 
