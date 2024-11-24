8fb6214e9d626a68df7c431145e74c2f47d60608
BoRamSon <onbrson1@gmail.com>
2024-11-24 15:36:22 +0900

    feat(validation): errorMessage 상수화 / 정규표현식을 통해 입력 형식을 파악하여 문제 시 에러 발생하는 함수 생성

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,6 +1,6 @@
 const ERROR_MESSAGES = {
-  EMPTY_VLAUE: "[ERROR] 값이 존재하지 않습니다.",
-  
+  EMPTY_VLAUE: "[ERROR] 값이 존재하지 않습니다. 다시 입력해 주세요.",
+  VALIDATE_REGEXP: "[ERROR] 형식이 올바르지 않습니다. 다시 입력해 주세요.",
 };
 
 export default ERROR_MESSAGES;
---
+++ src/validation/functions/validateRegExp.js
@@ -0,0 +1,9 @@
+import ERROR_MESSAGES from "../../constants/errorMessages";
+
+const validateRegEx = (value, regExp) => {
+  const regExpTest = regExp.test(value);
+  if (!regExpTest) throw new ERROR(ERROR_MESSAGES.VALIDATE_REGEXP);
+  return true;
+};
+
+export default validateRegEx;
