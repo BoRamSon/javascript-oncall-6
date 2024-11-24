968bb613e6c469373bda75748b9d80ae86f857f2
BoRamSon <onbrson1@gmail.com>
2024-11-24 16:14:13 +0900

    feat(validation): errorMessage 상수화 / 월 입력 시 1월 ~ 12월 이외 입력 시 에러 발생하는 함수 생성

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,6 +1,7 @@
 const ERROR_MESSAGES = {
   EMPTY_VLAUE: "[ERROR] 값이 존재하지 않습니다. 다시 입력해 주세요.",
   VALIDATE_REGEXP: "[ERROR] 형식이 올바르지 않습니다. 다시 입력해 주세요.",
+  INPUT_POSSIBLE_MONTH: "[ERROR] 월 입력의 경우, 1월 ~ 12월까지만 입력이 가능합니다. 다시 입력해주세요."
 };
 
 export default ERROR_MESSAGES;
