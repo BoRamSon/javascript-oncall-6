57f141ea00825df921f86db457d26e5f0e02493d
BoRamSon <onbrson1@gmail.com>
2024-11-24 15:19:17 +0900

    feat(controller): 비상 근무를 배정할 월과 시작 요일을 입력받는다.

--- src/controller/OrganizationController.js
+++ src/controller/OrganizationController.js
@@ -4,10 +4,17 @@ class OrganizationController {
   constructor() {}
 
   organizeScheduling() {
-    this.input();
+    this.getMonthAndDay();
   }
 
-  
+  getMonthAndDay() {
+    const inputMonthAndDay = MissionUtils.Console.readLineAsync(
+      "비상 근무를 배정할 월과 시작 요일을 입력하세요> "
+    );
+    
+  }
+
+
 }
 
 export default OrganizationController;
