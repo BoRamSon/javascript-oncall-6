f698d9568670b1d6ed2b837416af6e5101ec97ef
BoRamSon <onbrson1@gmail.com>
2024-11-27 00:24:59 +0900

    feat(controller): 평일 비상 근무자 순서 입력받는 메서드  /  평일 비상 근무자 순서 입력값을 validation하는 메서드

--- src/controller/OrganizationController.js
+++ src/controller/OrganizationController.js
@@ -1,4 +1,8 @@
-import InputMonthAndDay from "../modules/InputMonthAndDay.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import InputMonthAndDay from '../modules/InputMonthAndDay.js';
+import validateEmpty from '../validation/functions/validateEmpty.js';
+import validateRegEx from '../validation/functions/validateRegExp.js';
+import validateEndComma from '../validation/functions/validateEndComma.js';
 
 class OrganizationController {
   constructor() {
@@ -7,24 +11,27 @@ class OrganizationController {
 
   async organizeScheduling() {
     await this.inputMonthAndDay.changeArray();
+    await this.inputWeekdayWorker();
   }
 
-  // 평일 비상 근무 순서를 입력받는다.
   async inputWeekdayWorker() {
     const weekdayWorker = await MissionUtils.Console.readLineAsync(
-      "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> "
+      '평일 비상 근무 순번대로 사원 닉네임을 입력하세요> '
     );
+    const stringType = String(weekdayWorker);
+    const noSpaceValue = stringType.replace(/ /g, '');
+    this.validationWeekday(noSpaceValue);
+    return noSpaceValue;
   }
 
-  validationWeekday() {
-    
+  validationWeekday(input) {
+    validateEmpty(input);
+    const regExPatturn = /^[가-힣](,[가-힣])*/;
+    validateRegEx(input, regExPatturn);
+    validateEndComma(input);
+
+    // const inputArray = makeArrayFromString(input);
   }
-
-  // 휴일(토요일, 일요일, 법정공휴일) 비상 근무 순서를 입력받는다.
-  async inputWeekendWorker() {
-
-  }
-
 }
 
 export default OrganizationController;
